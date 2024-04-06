211b12a1f6301f59708bcc4e6c5543a9bc4f2f66
Igor <42415653+carbon77@users.noreply.github.com>
2024-04-06 21:10:54 +0300

    Initial Commit

---
+++ src/main/kotlin/com/zakat/avitobackend/AvitoBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.zakat.avitobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvitoBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvitoBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/zakat/avitobackend/AvitoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zakat.avitobackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvitoBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
